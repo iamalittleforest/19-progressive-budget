f22016828731738ffc92a537fbfbd89e2cc0af20
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-23 19:13:04 -0700

    define cache variables

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const STATIC_CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/styles.css",
+  "/index.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
