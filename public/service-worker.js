9cacabf2174668134a5ae5f2fafb51e654c20736
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-23 19:43:14 -0700

    outline install, activate, and fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,26 @@
-const STATIC_CACHE_NAME = "static-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
+const STATIC_CACHE_NAME = 'static-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.webmanifest",
-  "/styles.css",
-  "/index.js",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  '/',
+  '/index.html',
+  '/index.js',
+  '/manifest.webmanifest',
+  '/styles.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
+
+// install event
+self.addEventListener('install', event => {
+
+});
+
+// activate event
+self.addEventListener('activate', event => {
+
+});
+
+// fetch event
+self.addEventListener('fetch', event => {
+
+});
