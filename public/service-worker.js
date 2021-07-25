3d12c7f44a955f60bd28578b71f1d288aa163bf3
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-25 12:22:40 -0700

    refactor for clarity and consistency

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,55 +15,46 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', event => {
 
   // precache transaction data
-  // event.waitUntil(
-  //   caches.open(RUNTIME_CACHE)
-  //     .then(cache => cache.add("/api/transaction"))
-  // );
+  event.waitUntil(
+    caches
+      .open(RUNTIME_CACHE)
+      .then(cache => cache.add("/api/transaction"))
+  );
 
-  // precache static assets
+  // precache static data
   event.waitUntil(
     caches
       .open(STATIC_CACHE)
       .then(cache => cache.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
   );
+
+  self.skipWaiting();
 });
 
 // activate event
 self.addEventListener('activate', event => {
 
   // remove old caches
-  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
   event.waitUntil(
     caches
       .keys()
-      .then(cacheNames => {
-        return cacheNames.filter(
-          cacheName => !currentCaches.includes(cacheName)
-        );
-      })
-      .then(cachesToDelete => {
+      .then(keyList => {
         return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete);
+          keyList.map(key => {
+            if (key !== STATIC_CACHE && key !== RUNTIME_CACHE) {
+              console.log("Old cache data removed");
+              return caches.delete(key);
+            }
           })
         );
       })
-      .then(() => self.clients.claim())
   );
+
+  self.clients.claim()
 });
 
 // fetch event
 self.addEventListener('fetch', event => {
-
-  // does not cache non GET requests or requests to other origins
-  // if (
-  //   event.request.method !== "GET" ||
-  //   !event.request.url.startsWith(self.location.origin)
-  // ) {
-  //   event.respondWith(fetch(event.request));
-  //   return;
-  // }
 
   // cache requests to api
   if (event.request.url.includes("/api/transaction")) {
@@ -76,7 +67,7 @@ self.addEventListener('fetch', event => {
             // clone if response is sucessful
             .then(response => {
               if (response.status === 200) {
-                cache.put(event.request, response.clone());
+                cache.put(event.request.url, response.clone());
               }
               return response;
             })
@@ -89,35 +80,16 @@ self.addEventListener('fetch', event => {
     return;
   }
 
-  // use cached response first 
+  // cached static files 
   event.respondWith(
     caches
-      .match(event.request)
-      .then(cachedResponse => {
-
-        // use cached response if request is in cache
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        // fetch and cache response if request is not in cache
-        return caches
-          .open(RUNTIME_CACHE)
-          .then(cache => {
-            return fetch(event.request)
-
-              // clone if response is sucessful
-              .then(response => {
-                if (response.status === 200) {
-                  cache.put(event.request, response.clone());
-                }
-                return response;
-              })
-
-              // get from cache if response is unsuccessful
-              .catch(err => cache.match(event.request));
-          })
-          .catch(err => console.log(err))
+      .open(CACHE_NAME)
+      .then(cache => {
+        return cache
+          .match(event.request)
+          .then(response => {
+            return response || fetch(event.request);
+          });
       })
       .catch(err => console.log(err))
   );
